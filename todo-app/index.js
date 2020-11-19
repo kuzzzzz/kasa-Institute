9d0b1b0641819749500f886e16af1633a686cab0
Atumah Daniel <atumahdaniel@gmail.com>
2020-11-19 11:41:23 +0100

    to do app completed

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,17 @@
 const inputValue = document.getElementById("text");
 const submit = document.getElementById("submit");
 const form = document.getElementById("form");
-
 const textBox = document.getElementById("text-box");
+
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    addText(todo);
+  });
+}
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,6 +22,7 @@ submit.addEventListener("click", () => {
 
   if (text) {
     addText();
+   
   } else {
     alert("Please enter something");
   }
@@ -20,25 +30,56 @@ submit.addEventListener("click", () => {
 
 function addText(textinput) {
   let text = `<span>${inputValue.value}</span>`;
+
+  if (textinput) {
+    text = `<span>${textinput.text}</span>`;
+  }
+
   const textEl = document.createElement("li");
   textEl.innerHTML = `
   ${text} 
-  <div>
+  <div class="tank">
     <input type="checkbox" class="check-it ">
     <input type="submit" value="remove" class="remove">
 </div>
 `;
+
   const checkbox = textEl.querySelector(".check-it");
   const remove = textEl.querySelector(".remove");
 
+  if (textinput && textinput.completed) {
+    checkbox.checked = true;
+    textEl.classList.add("completed");
+    updateLS();
+  }
+
   checkbox.addEventListener("click", () => {
     textEl.classList.toggle("completed");
+    updateLS();
   });
+
   remove.addEventListener("click", () => {
     textEl.remove();
+    updateLS();
   });
 
   textBox.appendChild(textEl);
 
   inputValue.value = "";
+
+  updateLS();
+}
+
+function updateLS() {
+  const todosEL = document.querySelectorAll("li");
+  const todos = [];
+
+  todosEL.forEach((elm) => {
+    todos.push({
+      text: elm.innerText,
+      completed: elm.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
