24c18db108be8f31163aedc65988b1176af79285
Atumah Daniel <atumahdaniel@gmail.com>
2020-11-27 13:16:00 +0100

    example folder for kasa computer

---
+++ starter-template/index.js
@@ -0,0 +1,37 @@
+// // creat a variable with text " Helo world
+//  I am lucky " and calculate "the value of " 50 -20 /40 
+//  and print everything to the console
+ 
+
+// var X =" Hello world, I am lucky";
+
+// var Y = 50 - 20 / 40;
+
+// var Z = " The value Y is = "
+
+
+// console.log(X + Z + Y );
+
+
+// Check if number is even or odd
+
+var checkNumber = 7;
+
+var result = checkNumber % 2;
+
+if( result == 0   ){
+        console.log("The Number " +  checkNumber + " is even and the remander is " + result);
+}else{
+    console.log("The Number " +  checkNumber + " is odd and the remander is " + result);
+}
+
+
+
+
+
+
+
+
+
+
+
