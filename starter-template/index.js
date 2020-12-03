0ed88b1e3541852f13d63990f2f47616eb5e696b
Atumah Daniel <atumahdaniel@gmail.com>
2020-12-03 23:33:28 +0100

    todays class wrk

--- starter-template/index.js
+++ starter-template/index.js
@@ -1,7 +1,6 @@
 // // creat a variable with text " Helo world
-//  I am lucky " and calculate "the value of " 50 -20 /40 
+//  I am lucky " and calculate "the value of " 50 -20 /40
 //  and print everything to the console
- 
 
 // var X =" Hello world, I am lucky";
 
@@ -9,29 +8,63 @@
 
 // var Z = " The value Y is = "
 
-
 // console.log(X + Z + Y );
-
 
 // Check if number is even or odd
 
-var checkNumber = 7;
+// var result = checkNumber % 2;
 
-var result = checkNumber % 2;
+// if (result == 0) {
+//   document.body.innerText = checkNumber;
+// } else {
+//   //   document.write(NO);
+// }
 
-if( result == 0   ){
-        console.log("The Number " +  checkNumber + " is even and the remander is " + result);
-}else{
-    console.log("The Number " +  checkNumber + " is odd and the remander is " + result);
-}
-
-
+// document.body.innerHTML = "";
 
 
 
 
 
+// var yourLevel;
+
+// switch ("poor") {
+//   case "rich":
+//     yourLevel = "5 million";
+//     break;
+//   case "poor":
+//     yourLevel = "1 million";
+//     break;
+//   case "average":
+//     yourLevel = "3 million";
+// }
+// document.getElementById("demo").innerHTML = "Your have " + yourLevel;
 
 
+
+// let x = 5;
+//  x = x+5;
+//  console.log(x)
+
+// {
+// // block scope
+
+//   let x = 2;
+//   x = x*4
+//   console.log(x)
+  
+// }
+
+
+// function jk (){
+//  let x = 12;
+//  console.log(x)
+// }
+
+// jk()
+
+
+
+// write a for, while and do while  loop to print out ur name 10 times
 
 
