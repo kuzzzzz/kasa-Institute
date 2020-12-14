7146dfd542c9030ade4cdced2219ee7726c57072
Atumah Daniel <atumahdaniel@gmail.com>
2020-12-14 23:02:13 +0100

    advanced php

---
+++ php/Advanced/createandwrite.php
@@ -0,0 +1,23 @@
+<?php
+
+$myfile = fopen("mytest.txt","w");
+
+?>
+
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+
+<?php
+$myfile = fopen("newfile.txt", "a++") or die("Unable to open file!");
+$txt = "Mickey Mouse\n";
+fwrite($myfile, $txt);
+$txt = "Minnie Mouse\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
